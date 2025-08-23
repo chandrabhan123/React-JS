ceba3a30343541a2f34fad7db22437ce408369cd
Chandrabhan <chandrabhan0603@gmail.com>
2025-08-23 21:31:02 +0530

    compoents

---
+++ my-react-app/src/Vehicle.jsx
@@ -0,0 +1,4 @@
+function Vehicle(props) {
+    return <h2>My Bike Name is: {props.bike}</h2>;
+}
+export default Vehicle;
--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// import { StrictMode } from 'react'
+import { createRoot } from "react-dom/client";
+// import './index.css'
+// import App from './App.jsx'
+import Vehicle from "./Vehicle.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function Car(props) {
+  return <h2>I am a {props.brand} Car!</h2>;
+}
+
+function Garage() {
+  return (
+    <>
+      <h1>Who lives in my Garage?</h1>
+      <Car brand="Ford" />
+      <Car brand="BMW" />
+      <Car brand="Audi" />
+      <Vehicle bike="Honda" />
+    </>
+  );
+}
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <Garage />
+);
